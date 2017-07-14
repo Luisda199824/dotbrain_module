32fcc03773bc5b0cd0fb03f95a40ddf0ba7dffeb
Luis David Ocampo Aguirre <Luisda199824@gmail.com>
2017-07-14 15:30:55 -0500

    Organización inicial

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'dotbrain_test_module',
+  packages = ['dotbrain_test_module'],
+  version = '0.1',
+  description = 'Test PyPy',
+  author = 'DotBrain developers',
+  author_email = 'lucho@diegonaranjo.co',
+  url = 'https://gitlab.com/luisda199824/dotbrain_test_module.git', # use the URL to the github repo
+  download_url = 'https://gitlab.com/luisda199824/dotbrain_test_module.git/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
